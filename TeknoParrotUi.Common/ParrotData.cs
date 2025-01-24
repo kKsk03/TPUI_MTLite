b818a1b9300c346dbabba21365dec625662a74b1
kKsk <alonzomax1210@gmail.com>
2025-01-24 21:16:17 +0800

    Button height fix
    and setting default value changes

--- TeknoParrotUi.Common/ParrotData.cs
+++ TeknoParrotUi.Common/ParrotData.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace TeknoParrotUi.Common
+﻿namespace TeknoParrotUi.Common
 {
     public class ParrotData
     {
@@ -16,17 +14,17 @@ namespace TeknoParrotUi.Common
         public string PauseGameKey { get; set; } = "0x13";
 
         public bool SaveLastPlayed { get; set; }
-        
+
         public bool UseDiscordRPC { get; set; }
         public bool SilentMode { get; set; }
-        public bool CheckForUpdates { get; set; } = true;
+        public bool CheckForUpdates { get; set; } = false;
 
         public bool ConfirmExit { get; set; } = true;
-        public bool DownloadIcons { get; set; } = true;
+        public bool DownloadIcons { get; set; } = false;
         public bool UiDisableHardwareAcceleration { get; set; } = false;
 
         public string UiColour { get; set; } = "lightblue";
-        public bool UiDarkMode { get; set; } = false;
-        public bool UiHolidayThemes { get; set; } = true;
+        public bool UiDarkMode { get; set; } = true;
+        public bool UiHolidayThemes { get; set; } = false;
     }
 }
